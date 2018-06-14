a2e55efb2fff16bb6f6f9d9dd6b76e5d982c5ec2
Clement Mindjimba <30572198+cmindjimba@users.noreply.github.com>
2018-06-14 08:20:52 -0400

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,19 @@
+/*
+* Author: CLEMENT MINDJIMBA
+* Student Number: 0336848
+*
+* Tuesday,December 12
+* Project 2, Tic-Tac Toe
+*/
+#include<ctime>
+#include"Game.h"
+
+int main() 
+{
+	srand((unsigned) time(0)); //Random number generator is initialized.
+	Game game; //Creates a Game object.
+	game.selectPlayers(); //Prompts the user for each of the two players.
+	game.play();        
+	game.announceWinner();
+	system("PAUSE");
+}
