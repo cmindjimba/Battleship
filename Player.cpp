259c20b87f9d2654d3ae0f624764a4189c5839ab
Clement Mindjimba <30572198+cmindjimba@users.noreply.github.com>
2018-06-14 08:21:13 -0400

    Create Player.cpp

---
+++ Player.cpp
@@ -0,0 +1,23 @@
+/*
+* Author: CLEMENT MINDJIMBA
+* Student Number: 0336848
+*
+* Tuesday,December 12
+* Project 2, Tic-Tac Toe
+*/
+#include "Player.h"
+
+Player::Player(const string & n, Piece p) : name(n), piece(p){}
+
+Piece Player::getPiece() const
+{
+	return piece;
+}
+
+string Player::getName() const
+{
+	return name;
+}
+
+Player::~Player()
+{ }
