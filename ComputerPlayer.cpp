d62a442f394f589193fa977be2a244f89823dc3c
Clement Mindjimba <30572198+cmindjimba@users.noreply.github.com>
2018-06-14 08:19:09 -0400

    Create ComputerPlayer.cpp

---
+++ ComputerPlayer.cpp
@@ -0,0 +1,13 @@
+/*
+* Author: CLEMENT MINDJIMBA
+* Student Number: 0336848
+*
+* Tuesday,December 12
+* Project 2, Tic-Tac Toe
+*/
+#include "ComputerPlayer.h"
+
+ComputerPlayer::ComputerPlayer(Piece p): Player("Computer", p){}
+
+ComputerPlayer::~ComputerPlayer()
+{ }
