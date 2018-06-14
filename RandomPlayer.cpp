1f7f307bacbfc3b0eaa121ab9cf5bf02595ac876
Clement Mindjimba <30572198+cmindjimba@users.noreply.github.com>
2018-06-14 08:21:59 -0400

    Create RandomPlayer.cpp

---
+++ RandomPlayer.cpp
@@ -0,0 +1,32 @@
+/*
+* Author: CLEMENT MINDJIMBA
+* Student Number: 0336848
+*
+* Tuesday,December 12
+* Project 2, Tic-Tac Toe
+*/
+#include "RandomPlayer.h"
+
+RandomPlayer::RandomPlayer(Piece piece):ComputerPlayer(piece)
+{ }
+
+void RandomPlayer::makeMove(Board & board) const
+{
+	bool moveIsMade = false;
+	while (!moveIsMade)
+	{
+		int squareNumber = rand() % 9;
+		if (board.isLegal(getPiece(), squareNumber))
+		{
+			board.makeMove(getPiece(), squareNumber);
+			moveIsMade = true;
+		}
+		else 
+		{
+			squareNumber = rand() % 9;
+		}
+	}
+}
+
+RandomPlayer::~RandomPlayer()
+{ }
